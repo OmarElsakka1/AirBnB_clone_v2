115b730c2efc3295306d419b4c3f12e02ba9d9c5
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2024-02-24 20:06:18 +0000

    Handling user sqlalchemy

--- models/user.py
+++ models/user.py
@@ -1,11 +1,30 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+"""This is user class"""
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
+class User(BaseModel, Base):
+    """ Handling User class in MySQL database.
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    Attributes:
+        __tablename__: for the table name, users
+        email: (sqlalchemy String): User's email address.
+        password (sqlalchemy String): User's password.
+        first_name (sqlalchemy String): User's first name.
+        last_name (sqlalchemy String): User's last name.
+        places (sqlalchemy relationship): User&Place relationship.
+        reviews (sqlalchemy relationship): User&Review relationship.
+
+    """
+
+    __tablename__ = "users"
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+        places = relationship("Place", backref="user", cascade="delete")
+        reviews = relationship("Review", backref="user", cascade="delete")
