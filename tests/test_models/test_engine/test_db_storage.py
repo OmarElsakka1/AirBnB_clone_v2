e73df2d7c01d0f58d7818ee07530d8900cf74277
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2024-02-24 22:00:25 +0000

    testing

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-"""Unittests for DBStorage class of AirBnb_Clone_v2"""
+"""This is the unittesting for DBStorage class for AirBnb_Clone_v2"""
 import unittest
 import pep8
 import os
@@ -24,7 +24,7 @@ class TestDBStorage(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        """Tests"""
+        """Setting up the Tests"""
         cls.user = User()
         cls.user.first_name = "Kev"
         cls.user.last_name = "Yo"
@@ -33,64 +33,64 @@ class TestDBStorage(unittest.TestCase):
 
     @classmethod
     def teardown(cls):
-        """at the end of the test this will tear it down"""
+        """This is teardown for the testing"""
         del cls.user
 
     def tearDown(self):
-        """teardown"""
+        """teardown as a method"""
         try:
             os.remove("file.json")
         except Exception:
             pass
 
     def test_pep8_DBStorage(self):
-        """Tests pep8 style"""
+        """Testing pep8 style"""
         style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/engine/db_storage.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
+        pep = style.check_files(['models/engine/db_storage.py'])
+        self.assertEqual(pep.total_errors, 0, "fix pep8")
 
     def test_all(self):
-        """tests if all works in DB Storage"""
+        """testing if all works in DB Storage"""
         storage = FileStorage()
-        obj = storage.all()
-        self.assertIsNotNone(obj)
-        self.assertEqual(type(obj), dict)
-        self.assertIs(obj, storage._FileStorage__objects)
+        objec = storage.all()
+        self.assertIsNotNone(objec)
+        self.assertEqual(type(objec), dict)
+        self.assertIs(objec, storage._FileStorage__objects)
 
     def test_new(self):
-        """test when new is created"""
+        """testing in case new one was created"""
         storage = FileStorage()
-        obj = storage.all()
-        user = User()
-        user.id = 123455
-        user.name = "Kevin"
-        storage.new(user)
-        key = user.__class__.__name__ + "." + str(user.id)
-        self.assertIsNotNone(obj[key])
+        objec = storage.all()
+        user_new = User()
+        user_new.id = 123455
+        user_new.name = "Kevin"
+        storage.new(user_new)
+        key = user_new.__class__.__name__ + "." + str(user_new.id)
+        self.assertIsNotNone(objec[key])
 
     def test_reload_dbtorage(self):
         """
-        tests reload
+        test reloading
         """
         self.storage.save()
         Root = os.path.dirname(os.path.abspath("console.py"))
         path = os.path.join(Root, "file.json")
-        with open(path, 'r') as f:
-            lines = f.readlines()
+        with open(path, 'r') as r:
+            lines = r.readlines()
         try:
             os.remove(path)
         except Exception:
             pass
         self.storage.save()
-        with open(path, 'r') as f:
-            lines2 = f.readlines()
+        with open(path, 'r') as r:
+            lines2 = r.readlines()
         self.assertEqual(lines, lines2)
         try:
             os.remove(path)
         except Exception:
             pass
-        with open(path, "w") as f:
-            f.write("{}")
+        with open(path, "w") as w:
+            w.write("{}")
         with open(path, "r") as r:
             for line in r:
                 self.assertEqual(line, "{}")
