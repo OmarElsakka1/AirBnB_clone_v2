4db403a8dc8a12509473778e8f488b3a51073688
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2024-02-24 20:31:56 +0000

    City - Place - User

--- models/place.py
+++ models/place.py
@@ -1,18 +1,96 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.review import Review
+from models.amenity import Amenity, place_amenity
+from os import getenv
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
+import models
 
 
-class Place(BaseModel):
-    """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+class Place(BaseModel, Base):
+    """Putting a Place for a MySQL database.
+
+    Attributes:
+        __tablename__ (str): Name of the MySQL table to store places.
+        city_id (sqlalchemy String): Place's city id.
+        user_id (sqlalchemy String): Place's user id.
+        name (sqlalchemy String): Name.
+        description (sqlalchemy String): Description.
+        number_rooms (sqlalchemy Integer): Number of rooms.
+        number_bathrooms (sqlalchemy Integer): Number of bathrooms.
+        max_guest (sqlalchemy Integer): Maximum number of guests.
+        price_by_night (sqlalchemy Integer): Price by night.
+        latitude (sqlalchemy Float): Place's latitude.
+        longitude (sqlalchemy Float): Place's longitude.
+        reviews (sqlalchemy relationship): User-Review relationship.
+        amenities (sqlalchemy relationship): User-Amenity relationship.
+        amenity_ids (list): id list of all linked amenities
+
+    """
+
+    __tablename__ = "places"
+
+    if getenv('HBNB_TYPE_STORAGE') == "db":
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(128))
+        number_rooms = Column(Integer, default=0)
+        number_bathrooms = Column(Integer, default=0)
+        max_guest = Column(Integer, default=0)
+        price_by_night = Column(Integer, default=0)
+        latitude = Column(Float)
+        longitude = Column(Float)
+        reviews = relationship("Review", backref="place", cascade="delete")
+        amenities = relationship('Amenity', secondary=place_amenity,
+                                 back_populates='place_amenities',
+                                 viewonly=False)
+        amenity_ids = []
+    else:
+        city_id = ''
+        user_id = ''
+        name = ''
+        description = ''
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
+        @property
+        def reviews(self):
+            """Get a list of all linked Reviews.
+            """
+
+            review_list = []
+
+            for review in models.storage.all(Review).values():
+                if review.place_id == self.id:
+                    review_list.append(review)
+
+            return review_list
+
+        @property
+        def amenities(self):
+            """Get and Set linked Amenities.
+            """
+
+            amenity_list = []
+
+            for amenity in models.storage.all(Amenity).values():
+                if amenity.id in self.amenity_ids:
+                    amenity_list.append(amenity)
+
+            return amenity_list
+
+        @amenities.setter
+        def amenities(self, value):
+            """Adding an Amenity.id to the amenity_ids
+            """
+
+            if type(value) == Amenity:
+                self.amenity_ids.append(value.id)
--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This is user class"""
+"""This module defines a class User"""
 from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import Column, String
