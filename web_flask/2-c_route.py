40d31820a67944bf2119b135daaf189a268922d4
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2024-03-24 17:02:55 +0000

    C route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+Starting the web application as Flask
 """
 
 from flask import Flask
@@ -9,13 +9,13 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    """returns Hello HBNB!"""
+    """Just return Hello HBNB!"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
+    """Just return HBNB"""
     return 'HBNB'
 
 
