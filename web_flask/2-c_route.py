32213b00d92ed9e2e7e831affca7a77f29bb2288
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2024-03-24 17:00:40 +0000

    C route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,8 +21,8 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
-    """ followed by the value of the text variable"""
+    """Display C followed by the value of the text variable"""
     return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')C display 
+    app.run(host='0.0.0.0', port='5000')
