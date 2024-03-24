41a96d36e51db4a8f66ffc86463a33e3bb6d8b41
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2024-03-24 17:11:49 +0000

    number route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Starting the web application in Flask
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Just return Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Just return HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """Display C, then the given text"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """Display Python,then the given text, or (is cool)"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def imanumber(n):
+    """display (n is a number) only if n is an integer"""
+    return "{:d} is a number".format(n) 
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
